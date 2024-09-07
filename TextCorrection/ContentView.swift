2c5e857b94252e2b44deb423f15b3b7ede4516c7
李元魁 <circleghost0723@gmail.com>
2024-09-07 15:31:36 +0800

    Initial Commit

---
+++ TextCorrection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextCorrection
+//
+//  Created by 李元魁 on 2024/9/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextCorrection/TextCorrectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextCorrectionApp.swift
+//  TextCorrection
+//
+//  Created by 李元魁 on 2024/9/7.
+//
+
+import SwiftUI
+
+@main
+struct TextCorrectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
