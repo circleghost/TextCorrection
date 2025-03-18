557d126ae011b928562f2983a59b9f838e8efe00
circleghost <circleghost0723@gmail.com>
2025-03-18 12:03:28 +0800

    刪除舊的Desktop路徑結構

--- Desktop/程式開發練習/錯字app/TextCorrection/TestFile.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-// 這是一個測試檔案，用於驗證 XcodeGen 能否自動將新增檔案加入到 Xcode 專案中
-class TestClass {
-    func testFunction() {
-        print("XcodeGen 測試成功！")
-    }
-} 
--- Desktop/程式開發練習/錯字app/TextCorrection/TextCorrectionError.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum TextCorrectionError: Error, @unchecked Sendable {
-    case apiKeyNotSet
-    case invalidResponse
-    case apiError(statusCode: Int)
-    case timeout
-    case encodingError
-}
